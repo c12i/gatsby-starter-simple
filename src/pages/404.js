9c7ef105e695042dcaf707da0b746c50e29d869e
Collins Muriuki <murerwacollins@gmail.com>
2020-04-13 23:50:26 +0300

    Improve 404 page appearance

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/SEO/seo"
-import Image from "../components/image/image"
+import { Container404 } from "../styled-components/404"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Image />
+    <Container404 title="404">404</Container404>
+    <p style={{textAlign: 'center'}}>I find your lack of navigation disturbing</p>
   </Layout>
 )
 
