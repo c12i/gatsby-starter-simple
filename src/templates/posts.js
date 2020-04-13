1693cb24d5dc1310ae344915adf697a88b51be98
Collins Muriuki <murerwacollins@gmail.com>
2020-04-13 10:17:59 +0300

    Add post template

---
+++ src/templates/posts.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout/layout"
+
+export default ({ pageContext: { frontmatter, html } }) => (
+  <Layout>
+    <h1>{frontmatter.title}</h1>
+    <div dangerouslySetInnerHTML={{__html: html}} />
+    <p>Author: {frontmatter.author}</p>
+  </Layout>
+)
