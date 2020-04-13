aaac1409c5d8b64633a231e3d9f29ad5dc15f761
Collins Muriuki <49271447+collinsmuriuki@users.noreply.github.com>
2020-04-14 01:36:47 +0300

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from "react"
-
 import Layout from "../components/layout/layout"
 import SEO from "../components/SEO/seo"
 import { Container404 } from "../styled-components/404"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout headless>
     <SEO title="404: Not found" />
-    <Container404 title="404">404</Container404>
-    <p style={{textAlign: 'center'}}>I find your lack of navigation disturbing</p>
+    <Container404>404</Container404>
+    <p style={{textAlign: 'center'}}>I find your lack of navigation disturbing...</p>
   </Layout>
 )
 
