1e969385a9b58a493f63f10b8e224032a824fc2e
Collins Muriuki <murerwacollins@gmail.com>
2020-04-13 10:18:09 +0300

    Update components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Image from "../components/image"
+import Layout from "../components/layout/layout"
+import SEO from "../components/SEO/seo"
+import Image from "../components/image/image"
 
 const NotFoundPage = () => (
   <Layout>
