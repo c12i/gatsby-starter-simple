35abe602377ee92bd93a2517ae656cdf2cf49113
Collins Muriuki <murerwacollins@gmail.com>
2020-05-03 00:47:03 +0300

    Query custom metadata in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
@@ -6,26 +7,43 @@ import SEO from "../components/seo/seo"
 import { Divider } from "../styled-components/index"
 import { SocialLinks } from "../styled-components/about"
 
-const SecondPage = () => {
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          bio
+          socials {
+            name
+            url
+          }
+        }
+      }
+    }
+  `)
+  const { siteMetadata: { bio, socials }  } = data.site;
+
+  console.log(bio, socials)
+
   return (
     <Layout>
       <SEO title="About" description="About Me" />
-      <p>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English
-      </p>
+      <p>{bio}</p>
       <SocialLinks>
-        <a href="#">Linkedin</a>
-        <Divider />
-        <a href="https://github.com/thecodemint/gatsby-starter-simple">
-          Github
-        </a>
+        {socials.map(social => {
+          if (socials.length === 1) return <a href={social.url}>{social.name}</a>
+          return (
+            <div key={social.url}>
+              <a href={social.url}>{social.name}</a>
+              {
+                socials.lastIndexOf(social) ? null : <Divider />
+              }
+            </div>
+          )
+        })}
       </SocialLinks>
     </Layout>
   )
 }
 
-export default SecondPage
+export default About
--- src/styled-components/about.js
+++ src/styled-components/about.js
@@ -2,4 +2,6 @@ import styled from "styled-components"
 
 export const SocialLinks = styled.div`
   text-align: center;
+  display: flex;
+  justify-content: center;
 `
