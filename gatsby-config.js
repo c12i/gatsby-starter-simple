9e60126d65dd342fed1a0486c953147dd46563f1
Collins Muriuki <murerwacollins@gmail.com>
2020-05-03 00:46:38 +0300

    Refactor configuration to nculde custom metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,86 +1,106 @@
-module.exports = {
-  siteMetadata: {
-    // Update the following values to what you please
-    title: `Simple Blog`,
-    description: `Kick off your next, great Gatsby project with this simple starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `Simple`,
-  },
-  pathPrefix: "/gatsby-starter-simple",
-  plugins: [
-    `gatsby-plugin-react-helmet`,
+const siteMetadata = {
+  // Update the following values to what you please
+  title: `Simple Blog`,
+  description: `Kick off your next, great Gatsby project with this simple starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+  author: `Simple`,
+  // Add your socials here
+  bio: `It is a long established fact that a reader will be distracted by the
+  readable content of a page when looking at its layout. The point of
+  using Lorem Ipsum is that it has a more-or-less normal distribution of
+  letters, as opposed to using 'Content here, content here', making it
+  look like readable English`,
+  socials: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+      name: "Github",
+      url: "https://github.com/thecodemint/gatsby-starter-simple",
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdowns`,
-        path: `${__dirname}/src/posts`,
-      },
+      name: "Linkedin",
+      url: "#",
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-simple`,
-        short_name: `simple`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#333`,
-        display: `minimal-ui`,
-        icon: `src/images/simple-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        // replace the shortname with your own
-        // how to get Disqus shortname https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-        shortname: `simple-blog-7`
-      }
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/
-        }
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-offline`,
   ],
+}
+
+const plugins = [
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdowns`,
+      path: `${__dirname}/src/posts`,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-transformer-remark`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-simple`,
+      short_name: `simple`,
+      start_url: `/`,
+      background_color: `#fff`,
+      theme_color: `#333`,
+      display: `minimal-ui`,
+      icon: `src/images/simple-icon.png`, // This path is relative to the root of the site.
+    },
+  },
+  {
+    resolve: `gatsby-plugin-disqus`,
+    options: {
+      // replace the shortname with your own
+      // how to get Disqus shortname https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+      shortname: `simple-blog-7`,
+    },
+  },
+  {
+    resolve: "gatsby-plugin-react-svg",
+    options: {
+      rule: {
+        include: /assets/,
+      },
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      // The property ID; the tracking code won't be generated without it
+      trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 0,
+      // Enables Google Optimize using your container Id
+      optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+      // Enables Google Optimize Experiment ID
+      experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+      // Set Variation ID. 0 for original 1,2,3....
+      variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+      // Any additional optional fields
+      sampleRate: 5,
+      siteSpeedSampleRate: 10,
+      cookieDomain: "example.com",
+    },
+  },
+  `gatsby-plugin-sass`,
+  `gatsby-plugin-offline`,
+]
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  pathPrefix: "/gatsby-starter-simple", // REMOVE IF NOT DEPLOYING TO GH-PAGES
+  plugins: plugins,
 }
